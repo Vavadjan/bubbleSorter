4a23f5b1ba66371ccf9e4a97908ec47ced4dd93e
Vavadjan <vavadjan@gmail.com>
2023-10-11 20:22:07 +0300

    !

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 
 namespace LbubbleSorter
 {
+
+
+
+
+
+
+
     internal class Program
     {
 
@@ -17,15 +24,15 @@ namespace LbubbleSorter
         static void Main(string[] args)
         {
             int limitRndNumber = 100; // верхний предел лимита генератора псевдослучайных чисел
-            int seedRndNumber = 100; // число seed
-            int limitArrayNumber = 300; // лимит колличество элементов в инициализаторе массива
-            int minArrayNumber = Int32.MaxValue; // минимальное число массива 
+            int seedRndNumber = 1; // число seed
+            int limitArrayNumber = 10; // лимит колличество элементов в инициализаторе массива
+            int bufferNumber = Int32.MaxValue; // минимальное число массива 
             int minArrayNumberIndex = 0; // индекс минимального элемента в массиве
             int arrayBufferNumber; // буфер для временного хранения числа во время перемещения
 
             int[] rndArray = new int[limitArrayNumber]; // резервируем место в памяти под массив
 
-            Random rndNumber = new Random(seedRndNumber); // создаем генератор случайных чисел
+            Random rndNumber = new Random(); // создаем генератор случайных чисел
 
             for (int i = 0; i < limitArrayNumber; i++) // блок заполнения массива случайными числами
             {
@@ -37,26 +44,27 @@ namespace LbubbleSorter
 
             for (int x = 0; x < limitArrayNumber; x++)
             {
+
                 Console.WriteLine("\n"); // переход на новую строку
-                minArrayNumber = Int32.MaxValue; // сброс переменной minArrayNumber
+                bufferNumber = Int32.MaxValue; // сброс переменной minArrayNumber
 
                 for (int i = 0 + x; i < limitArrayNumber; i++) // блок поиска наименьшего числа (из оставшихся в массиве)
                 {
-                    if (rndArray[i] < minArrayNumber)
+                    if (rndArray[i] < bufferNumber)
                     {
-                        minArrayNumber = (rndArray[i]);
+                        bufferNumber = (rndArray[i]);
                         minArrayNumberIndex = i;
                     }
                 }
 
-                if (minArrayNumber != rndArray[x]) // блок перестановки местами элементов массива
+                if (bufferNumber != rndArray[x]) // блок перестановки местами элементов массива
                 {
                     arrayBufferNumber = rndArray[minArrayNumberIndex];
                     rndArray[minArrayNumberIndex] = rndArray[x];
-                    rndArray[x] = minArrayNumber;
+                    rndArray[x] = bufferNumber;
                 }
 
-                Console.WriteLine("наименьшее число среди оставшихся - " + minArrayNumber); // выводим в консоль наименьшее число в данный момент
+                Console.WriteLine("наименьшее число среди оставшихся - " + bufferNumber); // выводим в консоль наименьшее число в данный момент
                 ShowArray(limitArrayNumber, rndArray); // вызываем метод showArray
             }
         }
